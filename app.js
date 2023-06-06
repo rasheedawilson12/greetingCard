200469a32be50bbe265fcea2460581f5795c29bb
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-06 16:38:24 -0500

    JavaScript updated

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let cardTitle = document.querySelector("h1");
 let card = document.querySelector(".card");
 
 const openCard = () => {
-  cardTitle.innerHTML = `<h1>4. Have an 'egg-stra' Happy Easter!</h1>`;
+  cardTitle.innerHTML = `<h1>Have an 'egg-stra' Happy Easter!</h1>`;
   card.style.width = "80%";
 };
