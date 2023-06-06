b1de29637db0453a08bd9d5c124d6fc1e4166e3e
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-06-06 16:33:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let cardTitle = document.querySelector("h1");
+let card = document.querySelector(".card");
+
+const openCard = () => {
+  cardTitle.innerHTML = `<h1>4. Have an 'egg-stra' Happy Easter!</h1>`;
+  card.style.width = "80%";
+};
